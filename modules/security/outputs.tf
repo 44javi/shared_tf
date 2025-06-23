1ab4716ae9809b54ef6b76d1c09025676b42982a
44javi <pachecojavier44@gmail.com>
2025-06-22 22:52:54 -0700

    removed files that were copied over by mistake and added outputs

--- modules/security/outputs.tf
+++ modules/security/outputs.tf
@@ -1,18 +1,4 @@
-# /modules/entra_id/outputs.tf
-
-
-output "tenant_id" {
-  value = data.azurerm_client_config.current.tenant_id
+output "keyvault_admin_object_id" {
+  value     = azurerm_role_assignment.key_vault.principal_id
+  sensitive = true
 }
-
-output "data_engineers_group_id" {
-  description = "Object ID of the Data Engineers security group"
-  value       = azuread_group.data_engineers.object_id
-}
-
-output "data_engineers_display_name" {
-  description = "The display name of the Data Engineers group"
-  value       = azuread_group.data_engineers.display_name
-}
-
-
--- outputs.tf
+++ outputs.tf
@@ -10,8 +10,3 @@ output "resource_group_name" {
   value       = azurerm_resource_group.management.name
 }
 
-output "keyvault_admin_object_id" {
-  value     = azurerm_role_assignment.key_vault.principal_id
-  sensitive = true
-}
-
